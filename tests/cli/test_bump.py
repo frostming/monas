d2a62522602aba077a60bccdf0937cd69e60a80a
Frost Ming <mianghong@gmail.com>
2022-04-17 17:46:28 +0800

    stable list comparison

--- tests/cli/test_bump.py
+++ tests/cli/test_bump.py
@@ -4,10 +4,10 @@ from monas.utils import run_command
 def test_bump_patch_version(test_project, cli_run):
     cli_run(["bump", "patch"], cwd=test_project, input="\n")
     result = cli_run(["list", "--long"], cwd=test_project)
-    output_lines = [line.strip().split() for line in result.output.splitlines()]
+    output_lines = sorted(line.strip().split() for line in result.output.splitlines())
     assert output_lines == [
-        ["foo", "0.0.1", "packages/foo"],
         ["bar", "0.0.1", "packages/bar"],
+        ["foo", "0.0.1", "packages/foo"],
         ["foo-more", "0.0.1", "extras/foo-more"],
     ]
     result = run_command(
--- tests/cli/test_list.py
+++ tests/cli/test_list.py
@@ -6,23 +6,23 @@ import pytest
 @pytest.mark.parametrize("command", ["ls", "list"])
 def test_list_packages(test_project, cli_run, command):
     result = cli_run([command], cwd=test_project)
-    output_packages = [line.strip() for line in result.output.splitlines()]
-    assert output_packages == ["foo", "bar", "foo-more"]
+    output_packages = sorted(line.strip() for line in result.output.splitlines())
+    assert output_packages == ["bar", "foo", "foo-more"]
 
 
 def test_list_packages_json_output(test_project, cli_run):
     result = cli_run(["list", "--json"], cwd=test_project)
-    output_packages = json.loads(result.output)
+    output_packages = sorted(json.loads(result.output), key=lambda p: p["name"])
     assert output_packages == [
-        {
-            "name": "foo",
-            "version": "0.0.0",
-            "path": (test_project / "packages/foo").as_posix(),
-        },
         {
             "name": "bar",
             "version": "0.0.0",
             "path": (test_project / "packages/bar").as_posix(),
+        },
+        {
+            "name": "foo",
+            "version": "0.0.0",
+            "path": (test_project / "packages/foo").as_posix(),
         },
         {
             "name": "foo-more",
@@ -34,9 +34,12 @@ def test_list_packages_json_output(test_project, cli_run):
 
 def test_list_packages_long_output(test_project, cli_run):
     result = cli_run(["list", "--long"], cwd=test_project)
-    output_lines = [line.strip().split() for line in result.output.splitlines()]
+    output_lines = sorted(
+        (line.strip().split() for line in result.output.splitlines()),
+        key=lambda p: p[0],
+    )
     assert output_lines == [
-        ["foo", "0.0.0", "packages/foo"],
         ["bar", "0.0.0", "packages/bar"],
+        ["foo", "0.0.0", "packages/foo"],
         ["foo-more", "0.0.0", "extras/foo-more"],
     ]
